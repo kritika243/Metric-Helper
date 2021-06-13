b90c60616b214c997f99b1d8d3b98b7b8363ae45
Kritika <24kritikasharma24@gmail.com>
2021-06-13 16:23:30 +0530

    changed the dropdown to more modern using combbox

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
-from tkinter import StringVar, font
+from tkinter import StringVar, font, ttk
+from tkinter.constants import CENTER
 
 root = tkinter.Tk()
 root.title('Metric Helper')
@@ -21,25 +22,24 @@ input_field = tkinter.Entry(root, width=20, font=field_font, borderwidth=3)
 output_field = tkinter.Entry(root, width=20, font=field_font, borderwidth=3)
 equal_label = tkinter.Label(root, text='=', font=field_font, bg=bg_color)
 
+input_field.insert(0, "Enter your quantity")
 input_field.grid(row=0, column=0, padx=10,pady=10)
 equal_label.grid(row=0, column=1,padx=10,pady=10)
 output_field.grid(row=0, column=2,padx=10, pady=10)
 
-# create dropdowns for metric values
+# create combobox for metric values
 metric_list =['femto', 'pico', 'nano', 'micro', 'milli', 'centi', 'base value', 'deca', 'hecto', 'kilo', 'mega', 'giga', 'tera', 'peta']
-input_field.insert(0, 'Enter your quntity')
-input_choice = StringVar()
-output_choice = StringVar()
-input_dropdown = tkinter.OptionMenu(root, input_choice, *metric_list)
-output_dropdown = tkinter.OptionMenu(root, output_choice, *metric_list)
+# we don't have to expand the metric list in values for combobox - no need of *metric_list
+input_combobox= ttk.Combobox(root, values=metric_list, font=field_font, justify=CENTER)
+output_combobox= ttk.Combobox(root, values=metric_list, font=field_font, justify=CENTER)
 to_label = tkinter.Label(root, text='to', font=field_font, bg=bg_color)
 
-input_dropdown.grid(row=1, column=0)
-to_label.grid(row=1, column=1)
-output_dropdown.grid(row=1, column=2)
+input_combobox.grid(row=1, column=0, padx=10, pady=10)
+to_label.grid(row=1, column=1, padx=10, pady=10)
+output_combobox.grid(row=1, column=2, padx=10, pady=10)
 
-input_choice.set('base value')
-output_choice.set('base value')
+input_combobox.set('base value')
+output_combobox.set('base value')
 
 # create the converion button
 convert_button = tkinter.Button(root, text='Convert', font=field_font, bg=button_color)
